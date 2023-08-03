8e4a841dcdc9b69dfde145d8e50f5f287f8d7703
rutujaneharkar <89297692+rutujaneharkar@users.noreply.github.com>
2023-08-03 23:09:53 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -1,3 +1,5 @@
+/*Given an array arr[] of non-negative integers and an integer sum, find a subarray that adds to a given sum. There may be more than one subarray with sum as the given sum, print first such subarray.*/
+
 #include<iostream>  
 using namespace std;
   
