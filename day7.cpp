deac3f51801f3d3a897d32ee6ba50fda1c915dbe
rutujaneharkar <89297692+rutujaneharkar@users.noreply.github.com>
2023-08-07 23:09:00 +0530

    Add files via upload

---
+++ day7.cpp
@@ -0,0 +1,29 @@
+/*rotate array left n times accept n from user*/
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n,arr[100];
+	cout<<"enter elements in array";
+	cin>>n;
+	for(int i=0;i<=n;i++){
+		cin>>arr[i];
+		
+		}
+	for(int i=0;i<=n;i++){
+		cout<<"\narray is:"<<arr[i];
+	
+	
+        }    cout<<"";
+int key;		
+cout<<"\nenter key";
+cin>>key;
+for(int k=key;k<=n;k++)
+       cout<<"\n"<<arr[k];
+	
+	
+	for(int k=0;k<key;k++)
+	    cout<<"\n"<<arr[k];
+			
+}
+	
